7e205d28fb8c628d662a88790c7c725516a987af
Ivy <ivyzhao335@gmail.com>
2025-10-16 21:58:35 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const slideshowEl = document.getElementById('slideshow_container')
 
+
 async function getLogos(url) {
     try {
         const response = await fetch(url)
@@ -14,18 +15,50 @@ async function getLogos(url) {
 function displayLogos(data) {
     for (const [brand, logos] of Object.entries(data.records)) {
         if (!brand.startsWith('a')) continue
+
+        const div = document.createElement('div')
+        div.classList.add('slides')
+
         const titleEl = document.createElement('p')
         titleEl.innerHTML = brand
-        slideshowEl.appendChild(titleEl)
+        div.appendChild(titleEl)
 
         const logoOne = logos[0]
         if (logoOne) {
             const img = document.createElement('img')
             img.src = logoOne.logo;
             img.alt = logoOne.name;
-            slideshowEl.appendChild(img)
+            div.appendChild(img)
         }
+
+        slideshowEl.appendChild(div)
     }
+
+    showSlides(slideIndex);
+}
+
+let slideIndex = 1;
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName('slides')
+    if (slides.length === 0) return;
+
+    if (n > slides.length) slideIndex = 1;
+    if (n < 1) slideIndex = slides.length;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
 }
 
 getLogos('https://www.logotypes.dev/all')
